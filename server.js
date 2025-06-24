037408bad1e725a4a600ed7bc327f1a8a5373d7d
dverma321 <151020798+dverma321@users.noreply.github.com>
2025-06-24 14:04:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://englishdaily.netlify.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
